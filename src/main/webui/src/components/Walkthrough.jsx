432575cef0f57dab889832baea19e98fe940faa5
Destiny Gaines <gainesjdestiny@gmail.com>
2025-04-04 14:19:52 -0400

    Updating walkthrough

--- src/main/webui/src/components/Walkthrough.jsx
+++ src/main/webui/src/components/Walkthrough.jsx
@@ -5,9 +5,10 @@
     return (
       <div className="container">
         <h1>Walkthrough</h1>
-                go to <a className="link" href="https://www.instagram.com/accounts/access_tool/" target="_blank" rel="noopener noreferrer">Instagram</a> and download your data
+                go to <a className="link" href="https://accountscenter.instagram.com/info_and_permissions/dyi/" target="_blank" rel="noopener noreferrer">Instagram</a> and download your data
                 <br />
-                make sure you download it in JSON format
+                make sure you download it in JSON format. This process make take a day or two depending on what you chose to download.
+                I recommend downloading "Some of your information" -> "Followers and following" under "Connections" and selecting from "All time".
                 <br />
         <Link to="/" className="link">Go back</Link>
       </div>
