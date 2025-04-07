0ed3e7fea238c480949c938010988fefb1002b15
Destiny <dgaines92@gmail.com>
2025-04-07 18:01:36 -0400

    updating walkthrough to make sense

--- src/main/webui/src/components/Walkthrough.jsx
+++ src/main/webui/src/components/Walkthrough.jsx
@@ -5,11 +5,21 @@
     return (
       <div className="container">
         <h1>Walkthrough</h1>
-                go to <a className="link" href="https://accountscenter.instagram.com/info_and_permissions/dyi/" target="_blank" rel="noopener noreferrer">Instagram</a> and download your data
-                <br />
-                make sure you download it in JSON format. This process make take a day or two depending on what you chose to download.
-                I recommend downloading "Some of your information" -> "Followers and following" under "Connections" and selecting from "All time".
-                <br />
+        Go to <a className="link" href="https://accountscenter.instagram.com/info_and_permissions/dyi/" target="_blank" rel="noopener noreferrer">Instagram</a> to download youe data.
+        <ol>
+          <li>Go to your account's data download page.</li>
+          <li>Look for an option that says <strong>"Download your information"</strong> and click on it.</li>
+          <li>Make sure to <strong>select JSON format</strong> for the download.</li>
+          <li>
+            Under the available categories, go to 
+            <strong>"Some of your information"</strong> → 
+            <strong>"Connections"</strong> → 
+            <strong>"Followers and following"</strong>.
+          </li>
+          <li>When prompted for a date range, choose <strong>"All time"</strong> to ensure you get the complete list.</li>
+          <li>Submit your request.</li>
+        </ol>
+       <p><strong>⚠️ Note:</strong> It may take <em>a day or two</em> for the download to be ready, depending on the amount of data you've selected.</p>
         <Link to="/" className="link">Go back</Link>
       </div>
     );
